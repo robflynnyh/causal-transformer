b3e8ced312e2aea4c7be8920099c1f65b68e0cef
Rob <rjflynn2@sheffield.ac.uk>
2023-01-19 12:08:29 +0000

    updated to allow for previous states to be cached for incremental inference - see test function caching_test()

--- transformer.py
+++ transformer.py
@@ -4,6 +4,8 @@ from einops import rearrange, repeat
 from torch import einsum
 from torch.utils.checkpoint import checkpoint # # gradient/activation checkpointing
 from functools import partial
+import string
+from typing import Optional, Tuple, List, Dict, Union, Callable
 
 def exists(val):
     return val is not None
@@ -23,6 +25,7 @@ def shift(t, amount, mask = None):
     return F.pad(t, (0, 0, amount, -amount), value = 0.)
 
 class ShiftTokens(nn.Module):
+    '''from Phil Wang's x-transformers library'''
     def __init__(self, shifts, fn):
         super().__init__()
         self.fn = fn
@@ -41,8 +44,8 @@ class ShiftTokens(nn.Module):
 
 
 class DynamicPositionBias(nn.Module):
-    '''taken From Phil Wang's x-transformers library'''
-    def __init__(self, dim, *, heads, depth, log_distance = False, norm = False):
+    '''Adapted from Phil Wang's x-transformers library'''
+    def __init__(self, dim, *, heads, depth, log_distance = False, norm = False, activation=nn.SiLU):
         super().__init__()
         assert depth >= 1, 'depth for dynamic position bias MLP must be greater or equal to 1'
         self.log_distance = log_distance
@@ -52,39 +55,31 @@ class DynamicPositionBias(nn.Module):
         self.mlp.append(nn.Sequential(
             nn.Linear(1, dim),
             nn.LayerNorm(dim) if norm else nn.Identity(),
-            nn.ReLU()
+            activation()
         ))
 
         for _ in range(depth - 1):
             self.mlp.append(nn.Sequential(
                 nn.Linear(dim, dim),
                 nn.LayerNorm(dim) if norm else nn.Identity(),
-                nn.ReLU()
+                activation()
             ))
 
         self.mlp.append(nn.Linear(dim, heads))
 
-    def forward(self, n, device, dtype):
 
-        # get the (n x n) matrix of distances
-        seq_arange = torch.arange(n, device = device)
-        context_arange = torch.arange(n, device = device)
-        indices = rearrange(seq_arange, 'i -> i 1') - rearrange(context_arange, 'j -> 1 j')
-        indices += (n - 1)
+    def forward(self, pos, indices, device, dtype):
+        pos = pos.to(device=device, dtype=dtype)
         
-        # input to continuous positions MLP
-        pos = torch.arange(-n + 1, n, device = device, dtype = dtype)
-        pos = rearrange(pos, '... -> ... 1')
-
         if self.log_distance:
             pos = torch.sign(pos) * torch.log(pos.abs() + 1)  # log of distance is sign(rel_pos) * log(abs(rel_pos) + 1)
 
         for layer in self.mlp:
-            pos = layer(pos)
-
-        # get position biases        
+            pos = layer(pos) 
+      
         bias = pos[indices]
-        bias = rearrange(bias, 'i j h -> h i j')
+        #print(bias.shape)
+        bias = rearrange(bias, 'b i j h -> b h i j')
         return bias
 
 class ScaledSinuEmbedding(nn.Module):
@@ -163,44 +158,44 @@ class CosineAttention(nn.Module):
         return dots      
   
 
-    def attend(self, query, key, value, mask, pos_fn):
+    def attend(self, query, key, value, attn_mask, pos_bias):
         query, key = map(l2norm, (query, key))
-
+        
         dots = einsum('bhid,bhjd->bhij', query, key) * self.temperature
         dots = self.head_proj(dots, mode='pre')
 
-        dots += pos_fn(dots.shape[-1], device=dots.device, dtype=dots.dtype)
-        qkmask = ~mask
-        attn_mask = ~(rearrange(qkmask, "b n -> b () n ()") * rearrange(qkmask, "b n -> b () () n"))
-    
-        if self.causal: # create a regular causal mask
-            causal_mask = torch.ones(dots.shape[-2], dots.shape[-1], device=dots.device).triu(1).bool()
-            attn_mask = torch.logical_or(attn_mask, causal_mask)
-        
+        dots += pos_bias
+
         dots.masked_fill_(attn_mask, -torch.finfo(dots.dtype).max)
-    
+
         attn = self.activation(dots)
         attn = self.head_proj(attn, mode='post')
      
         attn = self.dropout(attn)
         return einsum("bhij,bhjd->bhid", attn, value)
 
+    @staticmethod
+    def attach_cache(kv, cache, cache_indices):
+        kv = torch.stack(kv, dim=0)
+        if cache is None:
+            return kv
+        zero_vector = torch.zeros_like(kv[:, :, :, :1, :])
+        kv_w_cache = torch.cat([cache, kv, zero_vector], dim=-2)
+        kv_w_cache = torch.gather(kv_w_cache, dim=-2, index=cache_indices) # we do this to remove unnecessary padding
+        return kv_w_cache
 
-    def forward(self, x, pos_fn, mask=None):
-        assert pos_fn is not None, 'pls provide a position function'
+    def forward(self, x, pos_bias, mask, cache=None, cache_indices=None):
         B, N, C, H, D = *x.shape, self.n_heads, self.head_dim
-        #print(x.shape, mask.shape)
-
-        if mask is None:
-            mask = torch.zeros(B, N, device=x.device, dtype=torch.bool)
-
-        q, k, v = self.qkv(x)
     
-        out = self.attend(q, k, v, mask, pos_fn)
+        q, k, v  = self.qkv(x)
+        kv = self.attach_cache([k, v], cache, cache_indices)
+        k, v = kv
+
+        out = self.attend(q, k, v, mask, pos_bias)
 
         out = rearrange(out, "b h n d -> b n (h d)")
         out = self.out_proj(out)
-        return out
+        return out, kv
 
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
@@ -246,6 +241,7 @@ class transformer(nn.Module):
         self.checkpoint_every_n = kwargs.get('checkpoint_every_n', 0)
         self.token_shift = kwargs.get('token_shift', False)
 
+        self.causal = causal
 
         self.temperature = nn.Parameter(torch.tensor(temperature), requires_grad=True) if shared_temperture else temperature
     
@@ -299,12 +295,75 @@ class transformer(nn.Module):
         condition = self.training and self.checkpoint_every_n != 0 and layer < self.depth - 1 and layer % self.checkpoint_every_n == 0
         return checkpoint(self.create_custom_forward(module), *args, **kwargs) if condition else module(*args, **kwargs)
 
-    def forward(self, x, mask=None, self_condtioning=None):
+    @staticmethod
+    def get_cache(cache, layer):
+        if cache is None:
+            return None
+        return cache['cache'][layer]
+
+    @staticmethod
+    def get_cache_indices(x_lens, cache_lens, cache_kv, x):  
+        # used later w/ gather to remove padding when cache is concatenated with current input to remove padding
+        max_new_len = (x_lens + cache_lens).max()
+
+        B, H, N, D = x.shape[0], 1, (x.shape[1] + cache_kv.shape[-2]), cache_kv.shape[-1]
+        indices = []
+        for i in range(B): # stinky for loop to sort out indices for gather 
+            cache_indices = torch.arange(cache_lens[i], device='cpu')
+            total_length = cache_lens[i] + x_lens[i]
+            diff_from_max_len = max_new_len - total_length
+            x_indices = torch.arange(x_lens[i]+diff_from_max_len, device='cpu') + cache_kv.shape[-2]
+            if diff_from_max_len > 0:
+                x_indices[-diff_from_max_len:] = N # last index will be used for padding
+            new_indices = torch.cat([cache_indices, x_indices])
+            indices.append(new_indices)
+
+        indices = torch.stack(indices, dim=0)
+        
+        indices = rearrange(indices, 'b n -> () b () n ()').expand(2, B, H,-1, D) # 2 for key and value
+        return indices.to(x.device)
+
+    def create_masks_and_positions(self, x, length, cache): 
+        x_len = length if length is not None else torch.tensor(x.shape[-2]).expand(x.shape[0])
+        cache_len = cache['cache_lengths'] if exists(cache) else 0
+        total_len = x_len + cache_len
+        kv_mask = torch.arange(total_len.max(), device=x.device).expand(len(total_len), -1) >= total_len.unsqueeze(-1)
+        q_mask = torch.arange(x_len.max(), device=x.device).expand(len(x_len), -1) >= x_len.unsqueeze(-1)
+        attn_mask = ~(rearrange(~q_mask, "b n -> b () n ()") * rearrange(~kv_mask, "b n -> b () () n"))
+        ##
+        ##
+        causal_mask = repeat(torch.arange(total_len.max()), 'i -> b r i', b=len(total_len), r=x_len.max())
+        cache_offset = cache_len[:,None,None] if exists(cache) else cache_len
+        diagonal_offset = torch.arange(x_len.max())[None,:,None]
+        ##
+        ## positional stuff ##
+        positional_grid = causal_mask - cache_offset - diagonal_offset 
+        pos = torch.arange(positional_grid.min(), positional_grid.max()+1).flip(0)[:,None]
+        min_cache_len = 0 if cache_len.__class__ == int else cache_len.min()
+        positional_indices = ((positional_grid*-1) + (total_len.max() - min_cache_len - 1)) # shift so zero is the smallest number
+        pos_bias = self.positional_bias(pos=pos, indices=positional_indices, dtype=x.dtype, device=x.device)
+        ## positional stuff ##
+        ##
+        if self.causal:
+            causal_mask = causal_mask >= (cache_offset + diagonal_offset + 1)
+            attn_mask = torch.logical_or(attn_mask, causal_mask[:,None])
+        ##
+        return q_mask, attn_mask, total_len, x_len, cache_len, pos_bias
+
+    def forward(self, x, length=None, self_condtioning=None, cache=None):
         intermediate_logits = []
+        cached_kvs = []
+
+        mask, attn_mask, total_lens, x_len, cache_len, pos_bias = self.create_masks_and_positions(x, length, cache)
+    
+        cache_indices = self.get_cache_indices(x_len, cache_len, cache['cache'], x) if exists(cache) else None
+    
         for i, (attn, ff) in enumerate(self.layers):
 
             x = self.token_shift(x)
-            x = self.checkpoint(i, attn, x, self.positional_bias, mask) + x
+            a_out, kv = self.checkpoint(i, attn, x, pos_bias, attn_mask, self.get_cache(cache, layer=i), cache_indices)
+            x = a_out + x
+            cached_kvs.append(kv)
             x = self.checkpoint(i, ff, x) + x   
 
             if i < self.depth - 1 and self_condtioning is not None:
@@ -313,8 +372,12 @@ class transformer(nn.Module):
 
         if len(intermediate_logits) > 0: # stack intermediate logits
             intermediate_logits = torch.stack(intermediate_logits, dim=0) # D x B x N x L
-    
-        return x, intermediate_logits
+
+        cached_kvs = torch.stack(cached_kvs, dim=0) if len(cached_kvs) > 0 else None
+        cached_kvs = {'cache_lengths': total_lens, 'cache': cached_kvs} if exists(cached_kvs) else None
+
+
+        return x, intermediate_logits, cached_kvs
 
 class shared_embedding_output_layer(nn.Module):
     '''Pass a embedding layer and then use this module as the output layer'''
@@ -396,11 +459,82 @@ class transformer_lm(nn.Module):
         return self_condition if (self.self_conditioning or self.intermediate_loss) and self.training else None
 
 
-    def forward(self, x, mask=None):
+    def forward(self, x, length=None, cache:Dict=None):
+        '''
+        x: [B, N] (embedding indices)
+        length: [B] (length of each sequence)
+        cache: {cache_lengths: [B, N], cache: [L, KV, B, H, N, D]} KV: key and value (2)
+        '''
         x = self.embedding(x)
-        x = self.abs_pos(x)
-        x, interim_logits = self.layers(x, mask=~mask if mask is not None else None, self_condtioning=self.self_condition_fn())
+        x = self.abs_pos(x) 
+  
+        x, interim_logits, cached_kvs = self.layers(x, length, self_condtioning=self.self_condition_fn(), cache=cache)
         x = self.post_norm(x)
         x = self.to_logits(x)
 
-        return  { 'out': x, 'interim_logits': interim_logits } if self.training else x
+        return  x, interim_logits, cached_kvs
+
+
+
+class CharacterTokenizer(): # only for testing!
+    def __init__(self):
+        self.vocab = ['#', '/'] + list(string.ascii_lowercase) + [' '] # bos/eos -> /, pad -> #
+        self.vocab_size = len(self.vocab)
+        self.token_to_id = {token: i for i, token in enumerate(self.vocab)}
+        self.id_to_token = {i: token for i, token in enumerate(self.vocab)}
+    
+    def __call__(self, text):
+        return self.tokenize(text)
+
+    def tokenize(self, text):
+        return [self.token_to_id[token] for token in text]
+
+def collate_fn(tensors:List[torch.Tensor], pad_token:int): # only for testing!
+    max_len = max([t.shape[0] for t in tensors])
+    lengths = torch.tensor([t.shape[0] for t in tensors])
+    padded_tensors = [torch.cat([t, torch.full((max_len - t.shape[0],), pad_token, dtype=t.dtype)], dim=0) for t in tensors]
+    return torch.stack(padded_tensors, dim=0), lengths
+
+
+@torch.no_grad()
+def caching_test():
+    tokenizer = CharacterTokenizer()
+    model = transformer_lm(
+        dim = 256,
+        vocab_size = tokenizer.vocab_size,
+        depth = 10,
+        heads = 1,
+        dim_head = 32,
+        dropout=0.0,
+        causal = True,
+        shared_kv = True,
+    )
+    model.eval()
+    # test batches to test caching
+    s1_b1, s2_b1, s3_b1 = torch.tensor(tokenizer('/hi')), torch.tensor(tokenizer('/buenos')), torch.tensor(tokenizer('/whats'))
+    s1_b2, s2_b2, s3_b2 = torch.tensor(tokenizer(' there')), torch.tensor(tokenizer(' dias')), torch.tensor(tokenizer(' up'))
+    s1_b3, s2_b3, s3_b3 = torch.tensor(tokenizer(' how')), torch.tensor(tokenizer(' captain')), torch.tensor(tokenizer(' donkey'))
+    s1_b4, s2_b4, s3_b4 = torch.tensor(tokenizer(' u/')), torch.tensor(tokenizer(' hook/')), torch.tensor(tokenizer(' man/'))
+    b1, b1_lengths = collate_fn([s1_b1, s2_b1, s3_b1], pad_token=tokenizer.token_to_id['#'])
+    b2, b2_lengths = collate_fn([s1_b2, s2_b2, s3_b2], pad_token=tokenizer.token_to_id['#'])
+    b3, b3_lengths = collate_fn([s1_b3, s2_b3, s3_b3], pad_token=tokenizer.token_to_id['#'])
+    b4, b4_lengths = collate_fn([s1_b4, s2_b4, s3_b4], pad_token=tokenizer.token_to_id['#'])
+    # comparsion set final states of above should be the same as these
+    f_1, f_2, f_3 = torch.tensor(tokenizer('/hi there how u/')), torch.tensor(tokenizer('/buenos dias captain hook/')), torch.tensor(tokenizer('/whats up donkey man/'))
+    fb, fb_lengths = collate_fn([f_1, f_2, f_3], pad_token=tokenizer.token_to_id['#'])
+
+    logits_s1, interim_logits, cached_kvs = model(b1, length=b1_lengths)
+    logits_s2, interim_logits, cached_kvs_s2 = model(b2, length=b2_lengths, cache=cached_kvs)
+    logits_s3, interim_logits, cached_kvs_s3 = model(b3, length=b3_lengths, cache=cached_kvs_s2)
+    logits_s4, interim_logits, cached_kvs_s4 = model(b4, length=b4_lengths, cache=cached_kvs_s3)
+    logits_fs, interim_logits, cached_kvs_fs = model(fb, length=fb_lengths)
+
+    print('shapes: ', cached_kvs_fs['cache'].shape, cached_kvs_s4['cache'].shape)
+    c_lens = cached_kvs_fs['cache_lengths']
+    mask = torch.arange(c_lens.max())[:,None] < c_lens[None,:]
+    mask = ~mask.T
+    mask = rearrange(mask, 'b i -> () () b () i ()')
+    fs_cache =  cached_kvs_fs['cache'].masked_fill(mask, 0)
+
+    assert torch.allclose(fs_cache, cached_kvs_s4['cache'], atol=0.001), 'failed check ): ): ):'
+    print('things are looking up !')
